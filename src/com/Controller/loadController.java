1050c5b6fc237e1033eb22d25b15f8b59ed0712f
Dhruv patel <33346480+dhruvbpatel@users.noreply.github.com>
2019-06-12 16:46:25 +0530

    path set for project execution

--- src/com/Controller/loadController.java
+++ src/com/Controller/loadController.java
@@ -1,5 +1,14 @@
 package com.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 public class loadController {
 
+	@RequestMapping(value="/")
+	public ModelAndView index(){
+		return new ModelAndView("admin/index");
+	}
 }
